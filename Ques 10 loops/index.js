82d6d0abd603891526da71af9da6784cfb0f1167
jayesh210 <jayeshpaliwal210@gmail.com>
2022-11-29 11:30:38 +0530

    Ques 10 done

---
+++ Ques 10 loops/index.js
@@ -0,0 +1,48 @@
+//Simple IF statement
+var marks=98;
+if(marks>78)
+{
+    var score =99;
+}
+console.log(marks)
+console.log(score);
+
+
+//Simple While loop
+
+
+var x=3
+while(x<5)
+{
+    console.log(x);
+    x++;
+}
+
+// Simple Do while Loop
+
+
+var y=0
+do
+{
+    console.log(y);
+    y++;
+}
+while(y>15);
+
+// In loop 
+
+
+const x=[1,,3,4,5]
+for(const y in x)
+{
+    console.log(y);
+}
+
+// OF Loop
+
+
+const x=[1,,3,4,5]
+for(const y of x)
+{
+    console.log(y);
+}
