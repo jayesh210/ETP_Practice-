2cb22967a5e9425498a5bb98a558c2e5f2154f32
jayesh210 <jayeshpaliwal210@gmail.com>
2022-11-29 11:16:05 +0530

    event program

---
+++ Ques 7 Event/event.js
@@ -0,0 +1,26 @@
+const EventEmitter = require('events');
+
+// Initializing event emitter instances
+var eventEmitter = new EventEmitter();
+
+var fun1 = (msg) => {
+	console.log("Message from fun1: " + msg);
+};
+
+var fun2 = (msg) => {
+	console.log("Message from fun2: " + msg);
+};
+
+// Registering fun1 and fun2
+eventEmitter.on('myEvent', fun1);
+eventEmitter.on('myEvent', fun2);
+
+// Removing listener fun1 that was
+// registered on the line 13
+eventEmitter.removeListener('myEvent', fun1);
+
+// Triggering myEvent
+eventEmitter.emit('myEvent', "Event occurred");
+
+// Triggering myEvent
+eventEmitter.emit('myEvent', "Event occurred");
